97596c7725611b9583dcc48dea893a9ec6df5940
Gloria Kuliś <gloria.kulis@student.pk.edu.pl>
2023-03-08 00:08:28 +0100

    wygenerowany szkielet

---
+++ pomidoro/src/main/java/ztpai/pomidoro/PomidoroApplication.java
@@ -0,0 +1,13 @@
+package ztpai.pomidoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomidoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomidoroApplication.class, args);
+	}
+
+}
---
+++ pomidoro/src/test/java/ztpai/pomidoro/PomidoroApplicationTests.java
@@ -0,0 +1,13 @@
+package ztpai.pomidoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomidoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
